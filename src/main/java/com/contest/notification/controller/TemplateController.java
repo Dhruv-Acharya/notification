454b8453e86b80309fdcbea22bc460879210a55d
rishabhkochar <rishabhkochar58@gmail.com>
2019-01-29 18:08:11 +0530

    Template CruD Done

--- src/main/java/com/contest/notification/controller/TemplateController.java
+++ src/main/java/com/contest/notification/controller/TemplateController.java
@@ -7,9 +7,9 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/template")
@@ -19,11 +19,34 @@ public class TemplateController {
     TemplateService templateService;
 
     @PostMapping
-    public ResponseEntity<String> addTemplate(TemplateDTO templateDTO){
+    public ResponseEntity<String> addTemplate(@RequestBody TemplateDTO templateDTO){
         Template template = new Template();
         BeanUtils.copyProperties(templateDTO, template);
         Template templateCreated = templateService.addTemplate(template);
-        return new ResponseEntity<>(templateCreated.getTemplateId(),HttpStatus.CREATED);
+        return new ResponseEntity<>(templateCreated.getTemplateId(),HttpStatus.OK);
+    }
+
+    @PutMapping("/{templateId}")
+    public ResponseEntity<String> updateTemplate(@PathVariable("templateId") String templateId,@RequestBody TemplateDTO templateDTO){
+        Template template = new Template();
+        BeanUtils.copyProperties(templateDTO, template);
+        template.setTemplateId(templateId);
+        Template templateCreated = templateService.updateTemplate(template);
+        return new ResponseEntity<>(templateCreated.getTemplateId(),HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{templateId}",method = RequestMethod.GET)
+    public ResponseEntity<TemplateDTO>  getOnetemplate(@PathVariable("templateId") String templateId){
+        Template template = templateService.findOneTemplate(templateId);
+        TemplateDTO templateDTO = new TemplateDTO();
+        BeanUtils.copyProperties(template, templateDTO);
+        return  new ResponseEntity<>(templateDTO,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{templateId}")
+    public ResponseEntity<Boolean> deleteTemplate(@PathVariable("templateId") String templateId){
+        templateService.deleteTemplate(templateId);
+        return new ResponseEntity<>(Boolean.TRUE,HttpStatus.OK);
     }
 
 
