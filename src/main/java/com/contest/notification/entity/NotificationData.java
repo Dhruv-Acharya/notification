03443867ee788ab52af775de7aa4dc9b9bd10640
rishabhkochar <rishabhkochar58@gmail.com>
2019-01-29 16:09:25 +0530

    Notification Data Entity Cretaed

--- src/main/java/com/contest/notification/entity/NotificationData.java
+++ src/main/java/com/contest/notification/entity/NotificationData.java
@@ -1,4 +1,11 @@
 package com.contest.notification.entity;
 
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = NotificationData.COLLECTION_NAME )
 public class NotificationData {
+
+    public static final String COLLECTION_NAME = "notificationdata";
+
 }
