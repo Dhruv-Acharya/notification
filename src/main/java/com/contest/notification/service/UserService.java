d9649ab3bb1bb854fca18a2e090670092aeba5d6
dhruvacharya <dhruv.acharya007@gmail.com>
2019-01-29 18:23:12 +0530

    User Controller

--- src/main/java/com/contest/notification/service/UserService.java
+++ src/main/java/com/contest/notification/service/UserService.java
@@ -6,8 +6,4 @@ public interface UserService {
     User addUser(User user);
     User findOne(String userId);
     User updateUser(User user);
-    User setAndroidDeviceId(User user, String androidDeviceId);
-    User setBrowserDeviceId(User user, String browserDeviceId);
-    User setFacebookPreference(User user, int preference);
-    User setContestPreference(User user, int preference);
 }
