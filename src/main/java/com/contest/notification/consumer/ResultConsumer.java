15a7dc7cfac07be9d5b90f6680e43d43eb60b77e
rishabhkochar <rishabhkochar58@gmail.com>
2019-01-31 13:25:50 +0530

    Exception handling and Database Dumb for Notifications

--- src/main/java/com/contest/notification/consumer/ResultConsumer.java
+++ src/main/java/com/contest/notification/consumer/ResultConsumer.java
@@ -2,13 +2,17 @@ package com.contest.notification.consumer;
 
 
 import com.contest.notification.dto.Header;
+import com.contest.notification.entity.NotificationData;
 import com.contest.notification.entity.User;
+import com.contest.notification.exception.FieldsCanNotBeEmpty;
 import com.contest.notification.notificationEnum.NotificationMedium;
 import com.contest.notification.notificationMedium.Sender;
 import com.contest.notification.notificationMedium.SenderFactory;
+import com.contest.notification.service.NotificationService;
 import com.contest.notification.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -21,15 +25,31 @@ public class ResultConsumer implements Consumer{
     @Autowired
     SenderFactory senderFactory;
 
+    @Autowired
+    NotificationService notificationService;
+
 
     @KafkaListener(topics="${result.kafka.topic}",containerFactory = "HeaderKafkaListenerContainerFactory")
-    public void receiveMessage(Header header) {
+    public void receiveMessage(Header header) throws FieldsCanNotBeEmpty {
+
         LOGGER.info("Received:"+ header);
+
+        if(header == null)
+            throw new FieldsCanNotBeEmpty("Header Cannot Be Empty");
+
+        if(header.getReceiver() == null || header.getNotificationMedium() == null || header.getNotificationType() == null ||
+                header.getNotificationTypeBody() == null || header.getTimeStamp() == null)
+            throw new FieldsCanNotBeEmpty("Header Fields Cannot Be Empty");
+
         User user= userService.findOne(header.getReceiver());
         for (NotificationMedium medium: header.getNotificationMedium()) {
             Sender sender = senderFactory.getInstance(medium);
             sender.send(header,processMessage(header),"LeaderBoard Updated",user);
         }
+
+        NotificationData notificationData = null;
+        BeanUtils.copyProperties(header,notificationData);
+        notificationService.addNotification(notificationData);
     }
 
     @Override
